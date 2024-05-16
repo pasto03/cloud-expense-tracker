323bdd058d553a83dd4d0aca767398c543dcb908
pasto03 <chumaolaw@gmail.com>
2024-05-16 21:45:20 +0800

    fix setupProxy target setting

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
-        '/api/auth',
+        '/api',
         createProxyMiddleware({
-            target: `${process.env.REACT_APP_SERVER_URL}/api/auth`,
-            changeOrigin: true,
-        })
-    );
-    app.use(
-        '/api/users',
-        createProxyMiddleware({
-            target: `${process.env.REACT_APP_SERVER_URL}/api/users`,
+            target: `${process.env.REACT_APP_SERVER_URL}`,
             changeOrigin: true,
         })
     );
