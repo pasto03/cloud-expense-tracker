0390175b91e77ea15dd30f23eaebb098991e7b3b
pasto03 <chumaolaw@gmail.com>
2024-05-16 22:06:58 +0800

    fix setupProxy target setting to main routes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
-        '/api',
+        '/api/auth',
         createProxyMiddleware({
-            target: `${process.env.REACT_APP_SERVER_URL}`,
+            target: `${process.env.REACT_APP_SERVER_URL}/api/auth`,
+            changeOrigin: true,
+        })
+    );
+    app.use(
+        '/api/users',
+        createProxyMiddleware({
+            target: `${process.env.REACT_APP_SERVER_URL}/api/users`,
             changeOrigin: true,
         })
     );
